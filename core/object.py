aeed7cff92a7c03a4dfbc100d9bc766b23e1235c
Ralph Wiedemeier <ralph@framefactory.ch>
2022-06-14 22:40:06 +0200

    add find object by data

--- core/object.py
+++ core/object.py
@@ -1,4 +1,4 @@
-from typing import List
+from typing import List, Any, Optional
 
 import bpy
 import bpy.types as bt
@@ -6,3 +6,14 @@ import bpy.types as bt
 
 def get_objects_by_type(type: str) -> List[bt.Object]:
     return [ obj for obj in bpy.data.objects if obj.type == type ]
+
+
+def get_objects_for_data(data: Any) -> List[bt.Object]:
+    return [ obj for obj in bpy.data.objects if obj.data == data ]
+
+
+def get_first_object_for_data(data: Any) -> Optional[bt.Object]:
+    for obj in bpy.data.objects:
+        if obj.data == data:
+            return obj
+    return None
